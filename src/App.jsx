0e7831b84bb192817905f0804c26b8918d860f36
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-02 04:30:32 +0530

    184. Reviews - Prev and Next

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import people from './data';
 
-// import { FaBeer } from 'react-icons/fa';
 import { FaChevronRight, FaChevronLeft, FaQuoteRight } from 'react-icons/fa';
 
 const App = () => {
   const [index, setIndex] = useState(0);
 
   const { id, name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return newIndex;
+    });
+  };
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return newIndex;
+    });
+  };
 
   return (
     <main>
@@ -21,6 +33,14 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   );
