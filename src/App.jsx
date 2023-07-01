bfc1546973ae81319d5fd160c5a0e142599ee762
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-02 01:50:57 +0530

    181. Reviews - Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import { useState } from 'react';
+import people from './data';
+
 const App = () => {
+  const [index, setIndex] = useState(0);
+  // const [index, setIndex] = useState(1);
+  // console.log(people[index]);
+  const { id, name, job, image, text } = people[index];
+  // console.log(name);
   return <h2>Reviews Starter</h2>;
 };
 export default App;
