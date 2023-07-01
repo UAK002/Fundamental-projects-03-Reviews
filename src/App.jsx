d8efaa0c4e681e40fdd2fe7c5b63bac9dd722c5c
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-02 02:35:34 +0530

    182. Reviews - React Icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import people from './data';
 
+import { FaBeer } from 'react-icons/fa';
+
 const App = () => {
   const [index, setIndex] = useState(0);
-  // const [index, setIndex] = useState(1);
-  // console.log(people[index]);
+
   const { id, name, job, image, text } = people[index];
-  // console.log(name);
-  return <h2>Reviews Starter</h2>;
+
+  return (
+    <div>
+      <h2>Reviews Starter</h2>;
+      <FaBeer className="beer" />
+    </div>
+  );
 };
 export default App;
